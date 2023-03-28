0e39efc7fd339d01bf14d4776437a9b80a91ac9a
TOMAS POLONSKY <47066838@ort>
2023-03-28 10:38:58 -0300

    agregar y tachar

--- codigo.js
+++ codigo.js
@@ -1,31 +1,67 @@
-varlistado = [];
+var listado = [];
 var ids = 0;
 
 function agregar()
 {    
     var input =  document.getElementById("input").value;
-    document.getElementById("input").value = "";
-    ids = ids+1;
-    var obj ={
-        id: ids,
-        titulo: input,
-        realizado: false
-    };
-
-    listado.push(obj);
-    console.log(listado)
-    // llamo a funcion refresh screen
-    PintarEnPantalla();
+    
+    if (document.getElementById("input").value.length == 0)
+    {
+        alert("Porfavor ingrese una tarea")
+        document.getElementById("input").value = "";
+    }
+    else {
+        ids = ids+1;
+        var obj ={
+            id: ids,
+            titulo: input,
+            realizado: false            
+        };
+    
+        listado.push(obj);
+        console.log(listado)
+        // llamo a funcion refresh screen
+        PintarEnPantalla();
+        document.getElementById("input").value = "";
+    }
+    
 }
 
 
 function PintarEnPantalla(){
-   var pantalla = document.getElementById("pantalla");
-    for (var i = 0; i <= listado.length; i++){
-        pantalla.innerHTML += listado[i];
-        
+   var pantalla = document.getElementById("tasks");
+   pantalla.innerHTML = "";
+   listado.map (item=>{
+
+    if (item.realizado = true){
+        var ñ = "<del>";
+        var l = "</del>";
     }
+
+    pantalla.innerHTML += `
+        <li>
+            <input type="checkbox" id="checkbox" onclick="realizar(${item.id})" />
+            ${ñ} ${item.titulo} ${l}
+        </li>
+    `
+
+
+   })
+  
 }
+
+function realizar(id)
+{
+    for (var i = 0; i < listado.length; i++)
+    {
+        if (listado[i].id == id){
+            listado.realizado = !listado.realizado;
+        }
+    }
+
+    PintarEnPantalla();
+}
+
 
 
 
